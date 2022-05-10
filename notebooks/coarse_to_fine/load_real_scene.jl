855b4da2aac6c5df3e1ddea1c88f7c1b263ea6ec
Nishad Gothoskar <nishadg@mit.edu>
2022-05-10 14:41:03 -0400

    correct scaling

--- notebooks/coarse_to_fine/load_real_scene.jl
+++ notebooks/coarse_to_fine/load_real_scene.jl
@@ -1,7 +1,8 @@
+import Pkg; Pkg.status()
+
 import InverseGraphics as T
 import Open3DVisualizer as V
 
-V.setup_visualizer()
 camera_intrinsics = T.GL.CameraIntrinsics(
 	640, 480,
 	507.8159715323307,
@@ -26,45 +27,32 @@ meshes = [
 ];
 
 data_path = joinpath(dirname(dirname(pathof(T))),"notebooks/coarse_to_fine/ibm_data")
-IDX = 345
+IDX = 218
 rgb_image = T.load_rgb(joinpath(data_path, "color_$(lpad(IDX,4,"0")).png"));
 
 d = T.load_depth(joinpath(data_path, "depth_$(lpad(IDX,4,"0")).png"));
-d = d ./ 1000.0
+d = d ./ 5.0
+
 
 c = T.GL.depth_image_to_point_cloud(d, camera_intrinsics);
 c = c[:,c[3,:] .> 1e-4]
-c = c[:, c[3,:] .< 1.1]
-
-V.reset_visualizer()
-V.viz(c);
+c = c[:, c[3,:] .< 200.1]
 
 best_eq, sub_cloud, _ = T.find_plane(c;threshold=0.01);
-
-V.reset_visualizer()
-V.viz(c);
-V.viz(sub_cloud;channel_name=:obs, color=T.I.colorant"red")
-
 cam_pose = T.camera_pose_from_table_eq(best_eq);
 c = T.move_points_to_frame_b(c, cam_pose);
+c = c[:, c[3,:] .> 1.0]
 
-V.reset_visualizer()
-V.viz(c);
-
-c = c[:, c[3,:] .> 0.01]
-V.reset_visualizer()
-V.viz(c);
-
-
-assign =T.dbscan_cluster(c; radius=0.03, min_cluster_size=50);
+assign =T.dbscan_cluster(c; radius=1.0, min_cluster_size=50);
 entities = T.get_entities_from_assignment(c, assign);
 @show length(entities)
 
+V.open_window()
+e = entities[4]
+V.clear()
+V.add(V.make_point_cloud(c ;color = T.I.colorant"red"));
+V.add(V.make_point_cloud(e ;color = T.I.colorant"blue"));
+V.sync()
+V.run()
 
-colors = T.I.distinguishable_colors(lenth(entities))
-X.open_window()
-for (col,e) in zip(colors, entities)
-	X.add(X.make_point_cloud(e;color=col))
-end
-X.run()
 
